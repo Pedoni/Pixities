e7bec3677d5391ba6ba4056b3a39ea438b9d2145
Emanuele Lamagna <elamagna@bsdsoftware.it>
2023-02-10 14:15:32 +0100

    Added first version of SplashScreen

---
+++ app/src/main/java/it/pedoni/pixities/ui/SplashActivity.kt
@@ -0,0 +1,27 @@
+package it.pedoni.pixities.ui
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.os.Build
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.delay
+
+@SuppressLint("CustomSplashScreen")
+class SplashActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            val splashScreen = installSplashScreen()
+            splashScreen.setKeepOnScreenCondition { true }
+        }
+        super.onCreate(savedInstanceState)
+        lifecycleScope.launchWhenCreated {
+            delay(1000)
+            startActivity(Intent(this@SplashActivity, MainActivity::class.java))
+            finish()
+        }
+    }
+}
